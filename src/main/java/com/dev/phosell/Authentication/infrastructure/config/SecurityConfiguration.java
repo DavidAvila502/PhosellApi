08133fc40b3855b85071232e13612f9b3682bf51
David Avila <davidavila6002@mail.com>
2025-05-05 13:11:23 -0600

    SecurityConfiguration updated

--- src/main/java/com/dev/phosell/Authentication/infrastructure/config/SecurityConfiguration.java
+++ src/main/java/com/dev/phosell/Authentication/infrastructure/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.dev.phosell.Authentication.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,9 +35,15 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**")
+
+                        // auth route
+                        .requestMatchers(HttpMethod.POST,"/auth/login", "/auth/register/client")
                         .permitAll()
-                        .requestMatchers("/api/client/").permitAll()
+
+                        .requestMatchers(HttpMethod.POST, "/auth/refresh","/auth/logout")
+                        .authenticated()
+
+                        // all other routes
                         .anyRequest()
                         .authenticated())
                 .sessionManagement(sess -> sess
