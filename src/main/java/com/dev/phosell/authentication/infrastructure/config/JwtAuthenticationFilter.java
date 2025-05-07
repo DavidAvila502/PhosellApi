00863a7f544c3aa567bd162e4631ee99290ba882
David Avila <davidavila6002@mail.com>
2025-05-07 14:26:58 -0600

    parse to CustomUserDetails added

--- src/main/java/com/dev/phosell/authentication/infrastructure/config/JwtAuthenticationFilter.java
+++ src/main/java/com/dev/phosell/authentication/infrastructure/config/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.dev.phosell.authentication.infrastructure.config;
 
 import com.dev.phosell.authentication.application.service.JwtService;
+import com.dev.phosell.authentication.domain.model.CustomUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,7 +10,6 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
@@ -57,7 +57,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
             if (userEmail != null && authentication == null) {
-                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+                CustomUserDetails userDetails = (CustomUserDetails) this.userDetailsService.loadUserByUsername(userEmail);
 
                 if (jwtService.isTokenValid(jwt, userDetails)) {
                     UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
